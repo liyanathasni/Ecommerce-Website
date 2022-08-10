e146d0b9afcf5ed908f3fe6e0eb2490dc357cdc7
navasck <abdul.navas@theretailinsights.com>
2022-08-10 18:30:50 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// loader
+
+const fadeout = () => {
+  const loaderwrapper = document.querySelector('.wrapper');
+  loaderwrapper.classList.add('fade');
+}
+
+window.addEventListener('load', fadeout);
+
+// go to top
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
